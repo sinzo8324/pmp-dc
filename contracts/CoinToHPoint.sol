64ca713e29adb5cfe1140056ef9a53d5125d8f7e
jhjeon1 <jhjeon1@aipluslab.ai>
2021-01-19 18:04:35 +0900

    Modify CoinToHPoint contract
    * Change the contract name (CoinToHPoint -> DCVault)
    * Remove to address on Pending request structure
    * Change event names
    * Change function names

--- contracts/CoinToHPoint.sol
+++ contracts/CoinToHPoint.sol
@@ -7,80 +7,80 @@ import 'openzeppelin-solidity/contracts/access/Ownable.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/IERC20.sol';
 import './RequestListLib.sol';
 
-contract CoinToHPoint is Ownable {
+contract DCVault is Ownable {
     using RequestListLib for RequestListLib.RequestIDList;
 
     struct PendingReq {
         address from;
-        address to;
         uint256 amount;
     }
 
-    uint256 cnt;
+    uint256 requestCnt;
     RequestListLib.RequestIDList internal reqIDList;
     mapping (bytes32 => PendingReq) pendingList;
 
-    event RequestRecorded(address indexed requester, address indexed toAddrOnKlaytn, uint256 amount);
-    event Finished(address indexed requester, address indexed toAddrOnKlaytn, uint256 amount, bytes32 txHash);
-    event CoinEmitted(address indexed toAddress, uint256 amount);
+    event DCLocked(bytes32 indexed requestID, address indexed requester, uint256 amount);
+    event Finished(bytes32 indexed requestID, address indexed requester, uint256 amount, bytes32 txHash);
+    event DCUnlocked(address indexed toAddress, uint256 amount);
     
-    address coinAddress;
+    address dcContractAddress;
 
-    function setCoin(address coinAddr) external onlyOwner {
-        coinAddress = coinAddr;
+    function setDCContractAddress(address contractAddress) external onlyOwner {
+        dcContractAddress = contractAddress;
     }
 
-    function getPendingList() external view returns (address[] memory, address[] memory, uint256[] memory) {
+    function getPendingList() external view returns (bytes32[] memory, address[] memory, uint256[] memory) {
         uint256 totalRequests = reqIDList.getLength();
+        bytes32[] memory requestIDList = new bytes32[](totalRequests);
         address[] memory fromAddrList = new address[](totalRequests);
-        address[] memory toAddrList = new address[](totalRequests);
         uint256[] memory amountList = new uint256[](totalRequests);
-        
         bytes32 reqID = reqIDList.getHead();
+
         for(uint256 i = 0; i < totalRequests; i++) {
+            requestIDList[i] = reqID;
             fromAddrList[i] = pendingList[reqID].from;
-            toAddrList[i] = pendingList[reqID].to;
             amountList[i] = pendingList[reqID].amount;
             reqID = reqIDList.getNext(reqID);
         }
 
-        return (fromAddrList, toAddrList, amountList);
+        return (requestIDList, fromAddrList, amountList);
     }
 
-    function requestHPointKlaytn(address toAddrOnKlaytn, uint256 amount) external {
-        require(toAddrOnKlaytn != address(0), 'target address cannot be zero');
+    function lockUpDC(address source, uint256 amount) public onlyOwner {
         require(amount !=0, 'amount cannot be zero');
-        require(IERC20(coinAddress).transferFrom(_msgSender(), address(this), amount), 'can not take coin from the account');
-
-        bytes32 reqID = keccak256(abi.encodePacked(_msgSender(), toAddrOnKlaytn, amount, cnt++));
-        PendingReq memory newReq;
+        require(IERC20(dcContractAddress).transferFrom(source, address(this), amount), 'can not take DC from the account');
+        bytes32 reqID = keccak256(abi.encodePacked(address(this), source, amount, requestCnt++));
         reqIDList.push(reqID);
-        newReq = PendingReq(_msgSender(), toAddrOnKlaytn, amount);
-        pendingList[reqID] = newReq;
+        pendingList[reqID] = PendingReq(source, amount);
 
-        emit RequestRecorded(_msgSender(), toAddrOnKlaytn, amount);
+        emit DCLocked(reqID, source, amount);
     }
 
-    function addTxHash(address[] calldata requester, address[] calldata to, uint256[] calldata amount, bytes32 txHash) external onlyOwner {
-        require(reqIDList.getLength() >= requester.length, 'input array length can not be bigger than request list length on the contract');
+    function addTxHash(bytes32[] calldata requestIDList, bytes32[] calldata txHash) external onlyOwner {
         bytes32 currentReqId = reqIDList.getHead();
         bytes32 nextReqId = reqIDList.getNext(currentReqId);
-        for(uint256 i = 0; i < requester.length; i++){
-            require(pendingList[currentReqId].from == requester[i] && pendingList[currentReqId].to == to[i] && pendingList[currentReqId].amount == amount[i], 'input should be matched with pending list on the contract');
+        for(uint256 i = 0; i < requestIDList.length; i++){
+            require(currentReqId == requestIDList[i], 'input should be matched with pending list on the contract');
+            address from = pendingList[currentReqId].from;
+            uint256 amount = pendingList[currentReqId].amount;
             delete pendingList[currentReqId];
             reqIDList.deleteReqID(currentReqId);
             currentReqId = nextReqId;
             nextReqId = reqIDList.getNext(currentReqId);
-            emit Finished(requester[i], to[i], amount[i], txHash);            
+            emit Finished(requestIDList[i], from, amount, txHash[i]);
         }
         reqIDList.updateHead(currentReqId);
     }
 
-    function emitErc20Token(address[] calldata destAddr, uint256[] calldata amount) external onlyOwner {
-        for(uint256 i = 0; i < destAddr.length; i++) {
-            IERC20(coinAddress).transfer(destAddr[i], amount[i]);
-            emit CoinEmitted(destAddr[i], amount[i]);
-        }
-        
+    function unlockDC(address destination, uint256 amount) external onlyOwner {
+        IERC20(dcContractAddress).transfer(destination, amount);
+        emit DCUnlocked(destination, amount);
     }
+
+    // function unlockDC(address[] calldata destination, uint256[] calldata amount) external onlyOwner {
+    //     for(uint256 i = 0; i < destination.length; i++) {
+    //         IERC20(dcContractAddress).transfer(destination[i], amount[i]);
+    //         emit DCUnlocked(destination[i], amount[i]);
+    //     }
+    // }
 }
