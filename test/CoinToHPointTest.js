c1dd10a9610ac0ef6013cf97ea3cc7184043080b
jhjeon1 <jhjeon1@aipluslab.ai>
2021-01-20 16:28:36 +0900

    Update the test script for dcVault

--- test/CoinToHPointTest.js
+++ test/CoinToHPointTest.js
@@ -1,47 +1,65 @@
 const { constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const keccak256 = require("web3-utils").soliditySha3;
 require('chai').should();
 
 const Proxy = artifacts.require('Proxy');
 const Erc20Logic = artifacts.require('Erc20Logic');
 const DataStorage = artifacts.require('DataStorage');
-const CoinToHPoint = artifacts.require('CoinToHPoint');
-const RequestListLib = artifacts.require('RequestListLib');
-require("chai")
-  .should();
+const DCVault = artifacts.require('DCVault');
 
-contract('CoinToHPoint', accounts => {
+require("chai").should();
+
+function generateRequestID(contractAddress, account, amount, count) {
+  return keccak256(
+    contractAddress,
+    account,
+    amount,
+    count
+  );
+}
+
+contract('DCVault', accounts => {
   before(async () => {
     this.erc20Proxy = await Proxy.new();
     this.erc20Logic = await Erc20Logic.new();
     this.dataStorage = await DataStorage.new();
-    this.coinToHPoint = await CoinToHPoint.new();
+    this.dcVault = await DCVault.new();
 
     await this.dataStorage.updateTokenDetails('Digital Currency', 'WON', '0');
     await this.dataStorage.transferOwnership(this.erc20Proxy.address);
     await this.erc20Proxy.addDataStorage(this.dataStorage.address);
     await this.erc20Proxy.updateLogicContract(this.erc20Logic.address);
-    await this.coinToHPoint.setCoin(this.erc20Proxy.address);
+    await this.dcVault.setDCContractAddress(this.erc20Proxy.address);
     await this.erc20Proxy.setInitialize(accounts[0], accounts[0], accounts[0], accounts[0]);
 
     this.erc20Token = await Erc20Logic.at(this.erc20Proxy.address);
     });
 
-  describe('RequestHPointKlaytn function', async () => {
-    it('toAddrOnKlaytn cannot be zero', async () => {
+  describe('setDCContractAddress', async () => {
+    it('Only the contract owner can use the function', async () => {
       await expectRevert(
-        this.coinToHPoint.requestHPointKlaytn(constants.ZERO_ADDRESS, '1000000', { from: accounts[1] }),
-        'target address cannot be zero'
+        this.dcVault.setDCContractAddress(constants.ZERO_ADDRESS, {from: accounts[1]}),
+        'Ownable: caller is not the owner'
+        );
+      });
+    });
+
+  describe('lockUpDC function', async () => {
+    it('Only the contract owner can use the function', async () => {
+      await expectRevert(
+        this.dcVault.lockUpDC(accounts[2], '0', { from: accounts[1] }),
+        'Ownable: caller is not the owner'
         );
       });
     it('amount cannot be zero', async () => {
       await expectRevert(
-        this.coinToHPoint.requestHPointKlaytn(accounts[2], '0', { from: accounts[1] }),
+        this.dcVault.lockUpDC(accounts[2], '0', { from: accounts[0] }),
         'amount cannot be zero'
         );
       });
     it('user account should have enough balance', async () => {
       await expectRevert(
-        this.coinToHPoint.requestHPointKlaytn(accounts[5], '100', {from: accounts[1]}),
+        this.dcVault.lockUpDC(accounts[1], '100', {from: accounts[0]}),
         'ERC20: transfer amount exceeds balance'
         );
       });
@@ -50,18 +68,18 @@ contract('CoinToHPoint', accounts => {
         await this.erc20Token.issue(accounts[i], '10000', {from: accounts[0]});
       }
       await expectRevert(
-        this.coinToHPoint.requestHPointKlaytn(accounts[5], '100', {from: accounts[1]}),
+        this.dcVault.lockUpDC(accounts[1], '100', {from: accounts[0]}),
         'ERC20: transfer amount exceeds allowance'
         );
       });
-    it('RequestRecorded event should be emitted', async () => {
+    it('DCLocked event should be emitted', async () => {
       for(let i = 1; i < 5; i++) {
-        await this.erc20Token.approve(this.coinToHPoint.address, '10000', {from: accounts[i]});
-        const receipt = await this.coinToHPoint.requestHPointKlaytn(accounts[i+4], '100', {from: accounts[i]});
-        expectEvent(receipt, 'RequestRecorded', {
-          requester: accounts[i],
-          toAddrOnKlaytn: accounts[i+4],
-          amount: '100',
+        await this.erc20Token.approve(this.dcVault.address, '10000', {from: accounts[i]});
+        const receipt = await this.dcVault.lockUpDC(accounts[i], '100', {from: accounts[0]});
+        expectEvent(receipt, 'DCLocked', {
+          requestID: generateRequestID(this.dcVault.address, accounts[i], '100', i - 1),
+          source: accounts[i],
+          amount: '100'
         });
       }
       });
@@ -69,10 +87,10 @@ contract('CoinToHPoint', accounts => {
 
   describe('getPendingList function', async () => {
     it('can read request list with detail information on the contract', async () => {
-      const result = await this.coinToHPoint.getPendingList();
+      const result = await this.dcVault.getPendingList();
       for(let i = 0; i < 4; i++) {
-        assert.equal(result[0][i], accounts[i + 1]);
-        assert.equal(result[1][i], accounts[i + 5]);
+        assert.equal(result[0][i], generateRequestID(this.dcVault.address, accounts[i + 1], '100', i));
+        assert.equal(result[1][i], accounts[i + 1]);
         assert.equal(result[2][i].toString(), '100');
       }      
       });
@@ -80,62 +98,91 @@ contract('CoinToHPoint', accounts => {
   
   describe('addTxHash function', async () => {
     it('only contract owner can call the function', async () => {
-      const requester = [accounts[1]];
-      const to = [accounts[5]];
-      const amount = ['100'];
+      const requestIDList = [generateRequestID(this.dcVault.address, accounts[1], '100', 0)];
       await expectRevert(
-        this.coinToHPoint.addTxHash(requester, to, amount, constants.ZERO_BYTES32, {from: accounts[1]}),
+        this.dcVault.addTxHash(requestIDList, [constants.ZERO_BYTES32], {from: accounts[1]}),
         'Ownable: caller is not the owner'
         );
       });
-    it('the input array length can not be bigger than length of request list on contract', async () => {
-      const requester = [accounts[2], accounts[3], accounts[4], accounts[5], accounts[6]];
-      const to = [accounts[2], accounts[3], accounts[4], accounts[5], accounts[6]];
-      const amount = ['100', '100', '100', '100', '100'];
+    it('length of input arrays should be same with each others', async () => {
+      let requestIDList = [];
+      let txHashList = [];
+      for(let i = 0; i < 4; i++) {
+        requestIDList.push(generateRequestID(this.dcVault.address, accounts[1 + i], '100', i));
+        txHashList.push(keccak256(i));
+      }
+      txHashList.push(keccak256(5));
       await expectRevert(
-        this.coinToHPoint.addTxHash(requester, to, amount, constants.ZERO_BYTES32, {from: accounts[0]}),
-        'input array length can not be bigger than request list length on the contract'
+        this.dcVault.addTxHash(requestIDList, txHashList, {from: accounts[0]}),
+        'length of input arrays should be same with each others'
         );
       });
-    it('the item order of input array should be match with request list on contract', async () => {
-      const requester = [accounts[2], accounts[3], accounts[4], accounts[5]];
-      const to = [accounts[6], accounts[7], accounts[8], accounts[9]];
-      const amount = ['100', '100', '100', '100'];
+    it('the input array length can not be bigger than length of request list on contract', async () => {
+      let requestIDList = [];
+      let txHashList = [];
+      for(let i = 0; i < 6; i++) {
+        requestIDList.push(generateRequestID(this.dcVault.address, accounts[1 + i], '100', i));
+        txHashList.push(keccak256(i));
+      }
       await expectRevert(
-        this.coinToHPoint.addTxHash(requester, to, amount, constants.ZERO_BYTES32, {from: accounts[0]}),
+        this.dcVault.addTxHash(requestIDList, txHashList, {from: accounts[0]}),
+        'length of input array cannot be bigger than pending list on the contract'
+        );
+      });
+    it('the requestIDs order of input array should be match with the list on contract', async () => {
+      let requestIDList = [];
+      let txHashList = [];
+      for(let i = 1; i < 4; i++) {
+        requestIDList.push(generateRequestID(this.dcVault.address, accounts[1 + i], '100', i));
+        txHashList.push(keccak256(i));
+      }
+      await expectRevert(
+        this.dcVault.addTxHash(requestIDList, txHashList, {from: accounts[0]}),
         'input should be matched with pending list on the contract'
         );
       });
-    it('finish event should be emitted', async () => {
-      const requester = [accounts[1], accounts[2], accounts[3]];
-      const to = [accounts[5], accounts[6], accounts[7]];
-      const amount = ['100', '100', '100'];
-      const receipt = await this.coinToHPoint.addTxHash(requester, to, amount, constants.ZERO_BYTES32, {from: accounts[0]});
-      for(let i = 0;  i < requester.length; i++){
+    it('Finished event should be emitted', async () => {
+      let requestIDList = [];
+      let txHashList = [];
+      for(let i = 0; i < 3; i++) {
+        requestIDList.push(generateRequestID(this.dcVault.address, accounts[1 + i], '100', i));
+        txHashList.push(keccak256(i));
+      }
+      const receipt = await this.dcVault.addTxHash(requestIDList, txHashList, {from: accounts[0]});
+      for(let i = 0;  i < requestIDList.length; i++){
         assert.equal(receipt.logs[i].event, 'Finished');
-        assert.equal(receipt.logs[i].args.requester, requester[i]);
-        assert.equal(receipt.logs[i].args.toAddrOnKlaytn, to[i]);
-        assert.equal(receipt.logs[i].args.amount, amount[i]);
+        assert.equal(receipt.logs[i].args.requestID, requestIDList[i]);
+        assert.equal(receipt.logs[i].args.source, accounts[i+1]);
+        assert.equal(receipt.logs[i].args.amount, '100');
       }
       });
     it('the remain requests should be stored in contracts', async () => {
-      let result = await this.coinToHPoint.getPendingList();
-      assert.equal(result[0][0], accounts[4]);
-      assert.equal(result[1][0], accounts[8]);
+      let result = await this.dcVault.getPendingList();
+      const requestID = generateRequestID(this.dcVault.address, accounts[4], '100', 3);
+
+      assert.equal(result[0][0], requestID);
+      assert.equal(result[1][0], accounts[4]);
       assert.equal(result[2][0].toString(), '100');
 
-      await this.coinToHPoint.requestHPointKlaytn(accounts[8], '100', {from: accounts[4]});
-      await this.coinToHPoint.requestHPointKlaytn(accounts[2], '100', {from: accounts[4]});
+      await this.dcVault.lockUpDC(accounts[2], '100', {from: accounts[0]});
+      await this.dcVault.lockUpDC(accounts[3], '100', {from: accounts[0]});
 
-      result = await this.coinToHPoint.getPendingList();
-      assert.equal(result[0][0], accounts[4]);
-      assert.equal(result[1][0], accounts[8]);
+      let requestIDList = [];
+      requestIDList.push(generateRequestID(this.dcVault.address, accounts[2], '100', 4));
+      requestIDList.push(generateRequestID(this.dcVault.address, accounts[3], '100', 5));
+
+      result = await this.dcVault.getPendingList();
+      assert.equal(result[0].length, 3);
+      assert.equal(result[1].length, 3);
+      assert.equal(result[2].length, 3);
+      assert.equal(result[0][0], requestID);
+      assert.equal(result[1][0], accounts[4]);
       assert.equal(result[2][0].toString(), '100');
-      assert.equal(result[0][1], accounts[4]);
-      assert.equal(result[1][1], accounts[8]);
+      assert.equal(result[0][1], requestIDList[0]);
+      assert.equal(result[1][1], accounts[2]);
       assert.equal(result[2][1].toString(), '100');
-      assert.equal(result[0][2], accounts[4]);
-      assert.equal(result[1][2], accounts[2]);
+      assert.equal(result[0][2], requestIDList[1]);
+      assert.equal(result[1][2], accounts[3]);
       assert.equal(result[2][2].toString(), '100');
       });
     });
